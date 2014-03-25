484ae2ef1e9fb01244b90dacd50f04b49c840d17
Justin Azoff <jazoff@illinois.edu>
2014-03-24 21:05:43 -0400

    lower timeout

--- dumbno.py
+++ dumbno.py
@@ -46,7 +46,7 @@ def is_expired(acl):
     if 'ago' not in acl:
         return True
 
-    return acl['ago'] > '0:10:00'
+    return acl['ago'] > '0:05:00'
 
 class ACLMgr:
     def __init__(self):
