4baea4d289596435b4ec875219dbd653238967b6
Justin Azoff <jazoff@illinois.edu>
2014-04-16 13:38:07 -0500

    implement per-port acl management

--- dumbno.py
+++ dumbno.py
@@ -39,25 +39,77 @@ def make_rule(s, d, proto="ip", sp=None, dp=None):
     return "%s %s %s %s %s" % (proto, a, ap, b, bp)
 
 class ACLMgr:
-    def __init__(self, logger):
+    def __init__(self, ports, logger):
+        self.acls = dict(("bulk_%s" % port, []) for port in ports)
+        self.ports = ports
         self.logger = logger
+
         self.min = 500
-        self.max = 10000
+        self.max = 100000
         self.seq = self.min + 1
         self.switch = Server( "https://admin:pw@host/command-api" )
+
+        self.setup()
         self.remove_expired()
+
+
+    def acl_exists(self, acl):
+        cmds = [
+            "enable",
+            "show ip access-lists %s" % acl,
+        ]
+        response = self.switch.runCmds(version=1, cmds=cmds, format='text')
+        acls = response[1]['output']
+        return bool(acls.strip())
+
+    def setup_acl(self, port):
+        acl = "bulk_%s" % port
+        if self.acl_exists(acl):
+            return True
+
+        self.logger.info("Setting up ACL for port %s", port)
+        cmds = [
+            "enable",
+            "configure",
+            "ip access-list %s" % acl,
+            "statistics per-entry",
+            "10 permit tcp any any fin",
+            "20 permit tcp any any syn",
+            "30 permit tcp any any rst",
+            "100001 permit ip any any",
+            "interface %s" % port,
+            "ip access-group %s in" % acl,
+        ]
+        response = self.switch.runCmds(version=1, cmds=cmds, format='text')
+
+    def setup(self):
+        for port in self.ports:
+            self.setup_acl(port)
 
     def refresh(self):
         cmds = [
             "enable",
-            "show ip access-lists bulk",
         ]
+        for acl in self.acls:
+            cmds.append("show ip access-lists %s" % acl)
         response = self.switch.runCmds(version=1, cmds=cmds, format='text')
-        acls = response[1]['output']
-        acls = parse_acl(acls)
-        self.used = set(x["seq"] for x in acls)
-        self.rules = set(x["rule"] for x in acls)
-        return acls
+        
+        self.all_seqs = set()
+        self.all_rules = set()
+        self.total_acls = 0
+        for acl, result in zip(self.acls, response[1:]):
+            acls = parse_acl(result['output'])
+            self.acls[acl] = acls
+
+            seqs  = set(x["seq"] for x in acls)
+            rules = set(x["rule"] for x in acls)
+
+            self.all_seqs.update(seqs)
+            self.all_rules.update(rules)
+
+            self.total_acls += len(acls)
+
+        return self.acls
 
     def dump(self, acls, op="CURRENT"):
         if not acls:
@@ -69,40 +121,41 @@ class ACLMgr:
     def calc_next(self):
         for x in range(self.seq, self.max) + range(self.min, self.seq):
             if x % 2 == 0: continue #i want an odd number
-            if x not in self.used:
+            if x not in self.all_seqs:
                 return x
         raise Exception("Too many ACLS?")
 
     def add_acl(self, src, dst, proto="ip", sport=None, dport=None):
         rule = make_rule(src, dst, proto, sport, dport)
 
-        if rule in self.rules:
+        if rule in self.all_rules:
             return False
 
-        self.seq = self.calc_next()
-
         cmds = [
             "enable",
             "configure",
-            "ip access-list bulk",
-            "%d deny %s" % (self.seq, rule),
         ]
+        self.seq = self.calc_next()
+        for acl in self.acls:
+            cmds.extend([
+                "ip access-list %s" % acl,
+                "%d deny %s" % (self.seq, rule),
+            ])
         self.logger.info("op=ADD seq=%s rule=%r" % (self.seq, rule))
         response = self.switch.runCmds(version=1, cmds=cmds, format='text')
-        self.rules.add(rule)
-        self.used.add(self.seq)
+        self.all_rules.add(rule)
+        self.all_seqs.add(self.seq)
         return True
 
-    def remove_acls(self, seqs):
-        if not seqs:
-            return
+    def remove_acls(self, to_remove):
         cmds = [
             "enable",
             "configure",
-            "ip access-list bulk",
         ]
-        for s in seqs:
-            cmds.append("no %s" % s)
+        for acl, entries in to_remove.items():
+            cmds.append("ip access-list %s" % acl)
+            for r in entries:
+                cmds.append("no %s" % r['seq'])
         self.logger.debug("Sending:" + "\n".join(cmds))
         response = self.switch.runCmds(version=1, cmds=cmds, format='text')
 
@@ -120,13 +173,19 @@ class ACLMgr:
     def remove_expired(self):
         acls = self.refresh()
 
-        to_remove = filter(self.is_expired, acls)
-        self.dump(to_remove, op="REMOVE")
-        to_remove = set(x['seq'] for x in to_remove)
+        to_remove = {}
+        to_remove_flat = []
+        for acl, entries in acls.items():
+            removable = filter(self.is_expired, entries)
+            to_remove[acl] = removable
+            to_remove_flat.extend(removable)
 
-        if to_remove:
+        self.dump(to_remove_flat, op="REMOVE")
+
+        if to_remove_flat:
             self.remove_acls(to_remove)
             acls = self.refresh()
+            self.logger.info("total_acls=%d", self.total_acls)
             #self.dump(acls)
         
 
@@ -175,11 +234,13 @@ class ACLClient:
             return None
 
 def main():
+    PORTS = ["Et1", "Et2", "Et3", "Et4", "Et5", "Et6", "Et7", "Et8"]
+
     format = '%(asctime)-15s %(levelname)s %(message)s'
     logging.basicConfig(level=logging.INFO, format=format)
     logger = logging.getLogger("dumbno")
     logger.info("Started")
-    mgr = ACLMgr(logger)
+    mgr = ACLMgr(PORTS, logger)
     svr = ACLSvr(mgr)
     svr.run()
 
